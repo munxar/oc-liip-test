f60f3eabe39129429670364ffd2207fe614a8315
munxar <sascha.aeppli@liip.ch>
2017-04-04 00:06:26 +0200

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Liip\Test;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
